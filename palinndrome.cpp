97c6962daea89249ddb24fed985624af929ed6f2
ssvmanikanth-sudo <ssvmanikanth@gmail.com>
2025-11-10 21:26:33 +0530

    Add files via upload

---
+++ palinndrome.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,rev=0,d,temp;
+	printf("enter a number");
+	scanf("%d",&n);
+	temp=n;
+	while(n>0)
+	{
+		d=n%10;
+		rev=rev*10+d;
+		n=n/10;
+	}if(rev==temp)
+	printf("it is a palindrome");
+	else
+	printf("not a palindrome");
+	return 0;
+}
