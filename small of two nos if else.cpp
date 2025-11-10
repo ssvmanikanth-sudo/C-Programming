b27fee62fcf33214612ca201d55feaf621ce18c8
ssvmanikanth-sudo <ssvmanikanth@gmail.com>
2025-11-10 21:35:27 +0530

    Add files via upload

---
+++ small of two nos if else.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a,b values");
+	scanf("%d%d",&a,&b);
+	if (a<b)
+	printf("%d is small",a);
+	else 
+	printf("%d is small",b);
+	return 0;
+}
