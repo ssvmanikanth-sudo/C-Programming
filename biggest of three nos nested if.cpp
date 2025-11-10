0351c048d1c772e7bdac5ccff0a45f86e61ae6a8
ssvmanikanth-sudo <ssvmanikanth@gmail.com>
2025-11-10 21:12:15 +0530

    Add files via upload

---
+++ biggest of three nos nested if.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter a,b,c values");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b)
+	{
+		if(a>c)
+		printf("a is big");
+		else
+		printf("b is big");
+	}
+	else
+	{
+		if(b>c)
+		printf("b is big");
+		else
+		printf("c is big");
+	}
+	return 0;
+}
