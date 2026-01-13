a968a7d766b001402d4e5999ae3cb1e013ca3a2c
ssvmanikanth-sudo <ssvmanikanth@gmail.com>
2026-01-13 14:41:41 +0530

    Add files via upload

---
+++ length of the string.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[20];
+	int l;
+	printf("enter string name");
+	gets(s);
+	l=strlen(s);
+	printf("length of the string=%d",l);
+	return 0;
+}
