57d1ba105e251adefd88026a6fdf6e6e44009140
SRISAILAPU S V MANIKANTH <ssvmanikanth@gmail.com>
2026-02-02 19:17:08 +0530

    Add files via upload

---
+++ total and average of n students.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+struct Student 
+ { 
+	 char name[50]; 
+	 int m1,m2,m3;
+	 float total,avg; 
+}; 
+int main() 
+ { 
+	 int n, i; 
+	 printf("Enter the number of students: "); 
+	 scanf("%d", &n); 
+	 struct Student s[n];  
+	 for(i=0;i<n;i++)  
+	 { 
+	printf("enter name,3 subject marks of student %d\n",i+1); 	
+        scanf("%s%d%d%d",s[i].name,&s[i].m1,&s[i].m2,&s[i].m3); 	
+	 } 
+	 for(i=0;i<n;i++) 
+	 { 
+         s[i].total=s[i].m1+s[i].m2+s[i].m3; 
+         s[i].avg=s[i].total/3.0; 
+        printf("Total and Average marks of Student %d=%.2f,%.2f\n",i+1,s[i].total,s[i].avg); 	 
+        } 
+		 return 0; 
+}
